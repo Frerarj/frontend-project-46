5c860590b7cae9baa63382708a123fdfb27942cd
Anastasia Simanova <frerarj@gmail.com>
2023-07-16 19:08:00 +0300

    Add tests for YML comparison

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -1,16 +1,18 @@
-import genDiff from '../src/index.js';
-
-import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const JSONexpected = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
+const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
-test('Test genDiff on plain JSON files', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(JSONexpected);
+test('genDiff on plain JSON files', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
+});
+
+test('genDiff on plain YML files', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expected);
 });
