379a12754889c6d50e6ac2e563fad4f1e3787fd0
Anastasia Simanova <frerarj@gmail.com>
2023-07-16 14:44:36 +0300

    Add test for JSON plain files

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -1,1 +1,16 @@
-// Some tests
+import genDiff from '../src/index.js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const JSONexpected = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
+
+test('Test genDiff on plain JSON files', () => {
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(JSONexpected);
+});
