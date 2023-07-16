7aed20927501805fb0a94495470afd0ba532dbbc
Anastasia Simanova <frerarj@gmail.com>
2023-07-16 19:26:15 +0300

    Add YAML parser

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import path from 'path';
 
 import compare from './compare.js';
 import makeFinalView from './formatting.js';
+import parser from './parsers.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const getFileExtention = (filepath) => path.extname(filepath);
 
 const genDiff = (file1, file2) => {
   const content1 = readFile(file1);
   const content2 = readFile(file2);
-  const data1 = JSON.parse(content1);
-  const data2 = JSON.parse(content2);
+  const data1 = parser(content1, getFileExtention(file1));
+  const data2 = parser(content2, getFileExtention(file2));
   const tree = compare(data1, data2);
 
   return makeFinalView(tree);
