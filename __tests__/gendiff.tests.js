80acfb0c92c703479e0517991f5a6b93cb6b705c
Anastasia Simanova <frerarj@gmail.com>
2023-07-20 10:31:21 +0300

    Remove plain file tests

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -9,25 +9,6 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
 
-const plainExpected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-
-test('genDiff on plain JSON files', () => {
-  const current = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(current).toEqual(plainExpected);
-});
-
-test('genDiff on plain YML files', () => {
-  const current = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  expect(current).toEqual(plainExpected);
-});
-
 test('genDiff on nested JSON files', () => {
   const current = genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.json'));
   const expected = readFile('expected-stylish.txt');
