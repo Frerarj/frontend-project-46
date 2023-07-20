2c1a2066d2920efbcbcc687065fc0e748eca3de3
Anastasia Simanova <frerarj@gmail.com>
2023-07-20 10:42:21 +0300

    Fix tests

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -9,26 +9,53 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
 
-test('genDiff on nested JSON files', () => {
-  const current = genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.json'));
-  const expected = readFile('expected-stylish.txt');
-  expect(current).toEqual(expected);
-});
+const stylish = [
+  ['nestedFile1.json', 'nestedFile2.json', 'expected-stylish.txt'],
+  ['nestedFile1.yml', 'nestedFile2.yml', 'expected-stylish.txt'],
+  ['nestedFile1.yml', 'nestedFile2.json', 'expected-stylish.txt'],
+];
 
-test('genDiff on nested YML files', () => {
-  const current = genDiff(getFixturePath('nestedFile1.yml'), getFixturePath('nestedFile2.yml'));
-  const expected = readFile('expected-stylish.txt');
-  expect(current).toEqual(expected);
-});
+const plain = [
+  ['nestedFile1.json', 'nestedFile2.json', 'expected-plain.txt'],
+  ['nestedFile1.yml', 'nestedFile2.yml', 'expected-plain.txt'],
+  ['nestedFile1.yml', 'nestedFile2.json', 'expected-plain.txt'],
+];
 
-test('genDiff plain formatting on nested JSON & YML files', () => {
-  const current = genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.yml'), 'plain');
-  const expected = readFile('expected-plain.txt');
-  expect(current).toEqual(expected);
-});
+const json = [
+  ['nestedFile1.json', 'nestedFile2.json', 'expected-json.txt'],
+  ['nestedFile1.yml', 'nestedFile2.yml', 'expected-json.txt'],
+  ['nestedFile1.yml', 'nestedFile2.json', 'expected-json.txt'],
+];
 
-test('genDiff JSON formatting on nested JSON & YML files', () => {
-  const current = genDiff(getFixturePath('nestedFile1.yml'), getFixturePath('nestedFile2.json'), 'json');
-  const expected = readFile('expected-json.txt');
-  expect(current).toEqual(expected);
-});
+test.each(stylish)(
+  'Find difference between %s and %s, make stylish',
+  (fileName1, fileName2, expectedResult) => {
+    const expected = readFile(expectedResult);
+    const filepath1 = getFixturePath(fileName1);
+    const filepath2 = getFixturePath(fileName2);
+    const result = genDiff(filepath1, filepath2);
+    expect(result).toEqual(expected);
+  },
+);
+
+test.each(plain)(
+  'Find difference between %s and %s, make plain',
+  (fileName1, fileName2, expectedResult) => {
+    const expected = readFile(expectedResult);
+    const filepath1 = getFixturePath(fileName1);
+    const filepath2 = getFixturePath(fileName2);
+    const result = genDiff(filepath1, filepath2, 'plain');
+    expect(result).toEqual(expected);
+  },
+);
+
+test.each(json)(
+  'Find difference between %s and %s, make json',
+  (fileName1, fileName2, expectedResult) => {
+    const expected = readFile(expectedResult);
+    const filepath1 = getFixturePath(fileName1);
+    const filepath2 = getFixturePath(fileName2);
+    const result = genDiff(filepath1, filepath2, 'json');
+    expect(result).toEqual(expected);
+  },
+);
